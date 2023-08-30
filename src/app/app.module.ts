17f18a787ad28beb47767a9fded79ece96a8dc9d
Pavan Kumar Yekabote <yekabotep@gmail.com>
2023-08-30 09:41:06 +0530

    Feat: Added deps and routing to main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './core/sidenav/sidenav.component';
+import { MainComponent } from './main/main/main.component';
+import { ProjectsComponent } from './core/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    MainComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
